9e2c22658660c9b28f2ef8c7c5e529a721a2b2b5
serdmi-kzhnv <serdmi-kzhnv@yandex-team.ru>
2025-05-12 21:43:57 +0300

    fix docker

--- frontend/coffee-shop/vite.config.ts
+++ frontend/coffee-shop/vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/coffee': {
-        target: 'http://localhost:8080',
+        target: process.env.VITE_API_URL || 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
       '/orders': {
-        target: 'http://localhost:8080',
+        target: process.env.VITE_API_URL || 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
+
